182b5734527dab30d3a7f5967aa57714c987629a
gabe-masterCode <111306815+gabe-masterCode@users.noreply.github.com>
2022-08-20 14:38:39 -0400

    Update carpick.js

--- carpick.js
+++ carpick.js
@@ -20,7 +20,7 @@ function getCookie(name) {
 }
 var curCookie;
 	//alert(document.cookie);
-	if(getCookie("car")!=""){
+	if((getCookie("car")!="")&&(getCookie("zip")!="")&&(getCookie("fee")!="")){
 		/*curCookie =  document.cookie;
 		var curCookies = curCookie.split(";");
 		alert(curCookies);
